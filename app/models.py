ad4146212df18060214ad70656fb581794d337fd
mkrol1997 <golotka333@gmail.com>
2023-08-18 01:11:07 +0200

    DB Models refactoring

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from . import db
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from sqlalchemy import ForeignKey
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(), unique=True)
+    password = db.Column(db.String())
+    name = db.Column(db.String())
+    comments = relationship("Comment", back_populates="author")
+    posts = relationship("BlogPost", back_populates="author")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey("users.id"))
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    author = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="parent_post")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    text = db.Column(db.String(), nullable=False)
+    author = relationship("User", back_populates="comments")
+    parent_post = relationship("BlogPost", back_populates="comments")
